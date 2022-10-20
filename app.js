96e9ca62e31f4815d66d3217480daf112e679903
Leandro Nazar <ljnazar@gmail.com>
2022-10-20 11:35:10 -0300

    Improve_Code

--- app.js
+++ app.js
@@ -15,98 +15,84 @@ let marginGood = `-15px 0px ${145 - heightScreen}px 0px`;
 let marginBad = `-5px 0px ${155 - heightScreen}px 0px`;
 let marginMiss = `0px 0px 0px 0px`;
 
-/////////////////////////////////////////////////////////
+const spyItem = (entries, observer) => {
+    entries.forEach(entry => {
+
+        console.log(entry);
+
+        if(observer.rootMargin === marginPerfect){
+            myDivTarget.style.backgroundColor = green;
+            if (!entry.isIntersecting) return;
+            myDivTarget.style.backgroundColor = blue;
+        }
+        else if(observer.rootMargin === marginGreat){
+            myDivTarget.style.backgroundColor = yellow;
+            if (!entry.isIntersecting) return;
+            myDivTarget.style.backgroundColor = green;
+        }
+        else if(observer.rootMargin === marginGood){
+            myDivTarget.style.backgroundColor = violet;
+            if (!entry.isIntersecting) return;
+            myDivTarget.style.backgroundColor = yellow;
+        }
+        else if(observer.rootMargin === marginBad){
+            myDivTarget.style.backgroundColor = red;
+            if (!entry.isIntersecting) return;
+            myDivTarget.style.backgroundColor = violet;
+        }
+        // observer.rootMargin === marginMiss
+        else{
+            myDivTarget.style.backgroundColor = red;
+            if (!entry.isIntersecting) return;
+            myDivTarget.style.backgroundColor = red;
+        }
+    });
+}
+
 let optionsPerfect = {  
     root: null,
     rootMargin: marginPerfect,  
     threshold: 1
 };
 
-let observerPerfect = new IntersectionObserver(entries => {  
-    entries.forEach(entry => {
-        console.log(entry)
-    
-        myDivTarget.style.backgroundColor = green;
-        if (!entry.isIntersecting) return;
-        myDivTarget.style.backgroundColor = blue;
-
-    });
-}, optionsPerfect);
-
-observerPerfect.observe(myDivTarget);
-/////////////////////////////////////////////////////////
 let optionsGreat = {  
     root: null,  
     rootMargin: marginGreat,  
     threshold: 1
 };
 
-let observerGreat = new IntersectionObserver(entries => {  
-    entries.forEach(entry => {
-        console.log(entry)
-    
-        myDivTarget.style.backgroundColor = yellow;
-        if (!entry.isIntersecting) return;
-        myDivTarget.style.backgroundColor = green;
-
-    });
-}, optionsGreat);
-
-observerGreat.observe(myDivTarget);
-/////////////////////////////////////////////////////////
 let optionsGood = {  
     root: null,  
     rootMargin: marginGood,  
     threshold: 1
 };
 
-let observerGood = new IntersectionObserver(entries => {  
-    entries.forEach(entry => {
-        console.log(entry)
-    
-        myDivTarget.style.backgroundColor = violet;
-        if (!entry.isIntersecting) return;
-        myDivTarget.style.backgroundColor = yellow;
-
-    });
-}, optionsGood);
-
-observerGood.observe(myDivTarget);
-/////////////////////////////////////////////////////////
 let optionsBad = {  
     root: null,  
     rootMargin: marginBad,  
     threshold: 1
 };
 
-let observerBad = new IntersectionObserver(entries => {  
-    entries.forEach(entry => {
-        console.log(entry)
-    
-        myDivTarget.style.backgroundColor = red;
-        if (!entry.isIntersecting) return;
-        myDivTarget.style.backgroundColor = violet;
-
-    });
-}, optionsBad);
-
-observerBad.observe(myDivTarget);
-/////////////////////////////////////////////////////////
 let optionsMiss = {  
     root: null,
     rootMargin: marginMiss,  
     threshold: 1
 };
 
-let observerMiss = new IntersectionObserver(entries => {  
-    entries.forEach(entry => {
-        console.log(entry)
-    
-        myDivTarget.style.backgroundColor = red;
-        if (!entry.isIntersecting) return;
-        myDivTarget.style.backgroundColor = red;
+let observerPerfect = new IntersectionObserver(spyItem, optionsPerfect);
+observerPerfect.observe(myDivTarget);
 
-    });
-}, optionsMiss);
+let observerGreat = new IntersectionObserver(spyItem, optionsGreat);
+observerGreat.observe(myDivTarget);
 
+
+let observerGood = new IntersectionObserver(spyItem, optionsGood);
+observerGood.observe(myDivTarget);
+
+
+let observerBad = new IntersectionObserver(spyItem, optionsBad);
+observerBad.observe(myDivTarget);
+
+
+let observerMiss = new IntersectionObserver(spyItem, optionsMiss);
 observerMiss.observe(myDivTarget);
